44bb52bfb48063a0f74cbf9930a796c12d3a8d38
VitalikMk <malyukovvitaliy4@gmail.com>
2023-09-20 18:20:19 +0300

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 fun main() {
+    val myArray = arrayOf(1, 2, 3,)
+    println(myArray.size)
+}
+
+
+/*
+fun main() {
     var x = 0
     var y = 0
     while (x < 5){
@@ -6,4 +13,9 @@ fun main() {
         print("$x$y")
         x = x + 1
     }
-}
+}*/
+/*
+fun main() {
+    val x = 5
+    var z: Long = x.toLong()
+}*/
