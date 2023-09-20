ad44b814c6567e3e715122b53e6b28c65b2534d0
VitalikMk <malyukovvitaliy4@gmail.com>
2023-09-20 18:32:50 +0300

    app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,16 @@
-fun main() {
-    val myArray = arrayOf(1, 2, 3,)
-    println(myArray.size)
+fun main(args: Array<String>){
+    val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
+    val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
+    val wordArray3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
+
+    val arraySize1 = wordArray1.size
+    val arraySize2 = wordArray2.size
+    val arraySize3 = wordArray3.size
+
+    val rand1 = (Math.random() * arraySize1).toInt()
+    val rand2 = (Math.random() * arraySize2).toInt()
+    val rand3 = (Math.random() * arraySize3).toInt()
+    
+    val phrase = "${wordArray1[rand1]} ${wordArray2[rand2]} ${wordArray3[rand3]}"
+    println(phrase)
 }
-
-
-/*
-fun main() {
-    var x = 0
-    var y = 0
-    while (x < 5){
-        y = x - y
-        print("$x$y")
-        x = x + 1
-    }
-}*/
-/*
-fun main() {
-    val x = 5
-    var z: Long = x.toLong()
-}*/
